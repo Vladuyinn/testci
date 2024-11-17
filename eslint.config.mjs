ff27d9a971b00896e41ef3fb8a0a3b424eb10d78
Vladuyinn <romaincrombetravail@gmail.com>
2024-11-17 15:53:54 +0100

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ["**/*.test.js"],
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.node, // Référence correcte pour globals@14
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-console": "error",
+    },
+  },
 ];
